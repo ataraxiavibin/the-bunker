a62ad995d8bd04627f33415988524a99dfdaf387
Ataraxia <aixarata@proton.me>
2026-05-09 22:03:49 +0200

    wacky implementation of a on-off switch

--- services/pods_connect/connect.py
+++ services/pods_connect/connect.py
@@ -3,6 +3,7 @@
 # connects to airpods
 
 import subprocess
+import argparse
 from shared.transmitter import send_to_bunker
 
 SOURCE = "pods_connect"
@@ -15,30 +16,40 @@ def connect_headphones() -> bool:
 
     return False
 
+def disconnect_headphones() -> bool:
+    result = subprocess.run(['bluetoothctl', 'disconnect'], capture_output=True, text=True)
+    if "Disconnection successful" in result.stdout:
+        return True
+
+    return False
+
 
 def check_connection() -> bool:
     result = subprocess.run(['bluetoothctl', 'devices', 'Connected'], capture_output=True, text=True)
     if MAC in result.stdout:
         return True
 
+    return False
 
 def main():
     if check_connection():
-        msg = "Already connected."
+        msg = "Already connected. Initiating disconnection."
         print(f"LOG: {msg}")
         send_to_bunker(SOURCE, "ok", {"message": msg})
-
-        return
-
-    if connect_headphones():
-        msg = "Connection successful."
-        print(f"LOG: {msg}")
-        send_to_bunker(SOURCE, "ok", {"message": msg})
+        if disconnect_headphones():
+            msg = "Successfuly disconnected."
+            print(f"LOG: {msg}")
+            send_to_bunker(SOURCE, "ok", {"message": msg})
     else:
-        msg = "Failed to connect."
-        print(f"LOG: {msg}")
-        send_to_bunker(SOURCE, "warning", {"message": msg})
-
+        if connect_headphones():
+            msg = "Connection successful."
+            print(f"LOG: {msg}")
+            send_to_bunker(SOURCE, "ok", {"message": msg})
+        else:
+            msg = "Failed to connect."
+            print(f"LOG: {msg}")
+            send_to_bunker(SOURCE, "warning", {"message": msg})
+  
 
 if __name__ == "__main__":
     main()

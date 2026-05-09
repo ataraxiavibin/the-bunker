c7a8c8d2ad2ad86bdca175bd6e4baae919569043
Ataraxia <aixarata@proton.me>
2026-05-09 22:19:58 +0200

    code refactor to follow DRY

--- services/pods_connect/connect.py
+++ services/pods_connect/connect.py
@@ -9,46 +9,47 @@ from shared.transmitter import send_to_bunker
 SOURCE = "pods_connect"
 MAC = "34:0E:22:C3:31:79"
 
-def connect_headphones() -> bool:
-    result = subprocess.run(['bluetoothctl', 'connect', MAC], capture_output=True, text=True)
-    if "Connection successful" in result.stdout:
-        return True
+def run_bt_command(*args) -> str:
+    result = subprocess.run(
+                            ['bluetoothctl', *args], 
+                            capture_output=True,
+                            text=True
+    )
+    return result.stdout
 
-    return False
+
+def log_and_send(status: str, msg: str) -> bool:
+    print(f"LOG: {msg}")
+    send_to_bunker(SOURCE, status, {"message": msg})
+    
+    return True # TODO: check if connection is valid and the bunker is online
+    # TODO: maybe add log_and_send to a shared file, since it's gonna be widely used
+
+
+def connect_headphones() -> bool:
+    return "Connection successful" in run_bt_command('connect', MAC)
+
 
 def disconnect_headphones() -> bool:
-    result = subprocess.run(['bluetoothctl', 'disconnect'], capture_output=True, text=True)
-    if "Disconnection successful" in result.stdout:
-        return True
-
-    return False
+    return "Disconnection successful" in run_bt_command('disconnect', MAC)
 
 
 def check_connection() -> bool:
-    result = subprocess.run(['bluetoothctl', 'devices', 'Connected'], capture_output=True, text=True)
-    if MAC in result.stdout:
-        return True
-
-    return False
+    return MAC in run_bt_command('devices', 'Connected')
 
 def main():
     if check_connection():
-        msg = "Already connected. Initiating disconnection."
-        print(f"LOG: {msg}")
-        send_to_bunker(SOURCE, "ok", {"message": msg})
+        log_and_send("ok", "Already connected. Initiating disconnection.")
+
         if disconnect_headphones():
-            msg = "Successfuly disconnected."
-            print(f"LOG: {msg}")
-            send_to_bunker(SOURCE, "ok", {"message": msg})
+            log_and_send("ok", "Successfully disconnected.")
+        else:
+            log_and_send("warning", "Failed to disconnect.") 
     else:
         if connect_headphones():
-            msg = "Connection successful."
-            print(f"LOG: {msg}")
-            send_to_bunker(SOURCE, "ok", {"message": msg})
+            log_and_send("ok", "Connection successful.")
         else:
-            msg = "Failed to connect."
-            print(f"LOG: {msg}")
-            send_to_bunker(SOURCE, "warning", {"message": msg})
+            log_and_send("warning", "Failed to connect.")
   
 
 if __name__ == "__main__":

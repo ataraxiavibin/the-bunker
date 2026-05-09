cb70000903ddb2a5e0ca8ebe2d745889955c03e1
Ataraxia <aixarata@proton.me>
2026-05-09 21:52:45 +0200

    removed unnecessary comment

--- services/pods_connect/connect.py
+++ services/pods_connect/connect.py
@@ -11,7 +11,7 @@ MAC = "34:0E:22:C3:31:79"
 def connect_headphones() -> bool:
     result = subprocess.run(['bluetoothctl', 'connect', MAC], capture_output=True, text=True)
     if "Connection successful" in result.stdout:
-        return True # here call to a log/transmitter function
+        return True
 
     return False
 
@@ -39,7 +39,6 @@ def main():
         print(f"LOG: {msg}")
         send_to_bunker(SOURCE, "warning", {"message": msg})
 
-    
 
 if __name__ == "__main__":
     main()

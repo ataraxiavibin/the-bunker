7cbcd2c0a366add441b19066cf99367a0e96daa8
Ataraxia <aixarata@proton.me>
2026-05-25 13:08:04 +0200

    add a transmitter log file, start logging exceptions

--- shared/transmitter.py
+++ shared/transmitter.py
@@ -4,6 +4,9 @@ import requests
 import os
 from typing import Dict, Any
 from dotenv import load_dotenv
+from loguru import logger
+
+logger.add("./logs/transmitter.log", rotation="2 MB", retention="7 days", level="INFO")
 
 load_dotenv()
 
@@ -21,4 +24,4 @@ def send_to_bunker(source: str, status: str, payload: Dict[str, Any]):
         response = requests.post(BUNKER_URL+"/event", json=data, headers=headers, timeout=5)
         response.raise_for_status()
     except Exception as e:
-        print(f"Failed to send event to bunker: {e}")
+        logger.warning(f"Failed to send event to bunker: {e}"

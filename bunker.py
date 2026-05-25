31c52b5f5313ff1f7a0cad3f9fa192116dfdd5ec
Ataraxia <aixarata@proton.me>
2026-05-25 14:13:32 +0200

    handle_call() function now sends it to a new agent server

--- bunker.py
+++ bunker.py
@@ -1,6 +1,7 @@
 # bunker.py
 
 import os
+import requests # it's gonna be a little bit painful to switch to a new async library..
 from fastapi import FastAPI, Header, HTTPException, Request
 from pydantic import BaseModel
 from typing import Dict, Any
@@ -13,6 +14,7 @@ app = FastAPI()
 
 load_dotenv()
 api_token = os.environ.get("API_TOKEN")
+agent_url = os.environ.get("AGENT_URL")
 
 class Event(BaseModel):
     source: str
@@ -28,7 +30,7 @@ class Call(BaseModel):
     target: Target
 
 @app.post("/event")
-async def handle_event(event: Event, x_token: str = Header(...), request: Request):
+async def handle_event(event: Event, request: Request, x_token: str = Header(...)):
     if x_token != api_token:
         logger.warning(f"Failed auth attempt from {request.client.host}")
         raise HTTPException(status_code=403, detail="Forbidden")
@@ -42,8 +44,28 @@ async def handle_ping():
     return {"status": "alive"}
 
 @app.post("/call")
-async def handle_call(call: Call, x_token: str = Header(...)):
-    pass # here: call agent   
+async def handle_call(call: Call, request: Request, x_token: str = Header(...)):
+    if x_token != api_token:
+        logger.warning(f"Failed auth attempt from {request.client.host}")
+        raise HTTPException(status_code=403, detail="Forbidden")
+
+    logger.info(f"Call from {call.source} to {call.target.service}: ACTION - {call.target.action}")
+
+    headers = {"x-token": api_token}
+    data = {
+        "source": call.source,
+        "target": call.target.model_dump()
+    }
+
+    try:
+        response = requests.post(agent_url+"/call", json=data, headers=headers, timeout=5)
+        response.raise_for_status()
+    except Exception as e:
+        logger.warning(f"Couldn't reach agent.py: {e}")
+        return {"status": "error"}
+
+    return {"status": "forwarded"}
+
 
 if __name__ == "__main__":
     import uvicorn

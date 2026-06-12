3c4d8dbaaf4f1841bb8b568a75c56e251a1101cf
Ataraxia <aixarata@proton.me>
2026-06-12 23:29:59 +0200

    chore(agent): add comprehensive logging

--- agent.py
+++ agent.py
@@ -42,11 +42,13 @@ MAPPINGS = {
 @app.post("/call")
 async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> Reply:
     if x_token != api_token:
+        logger.warning(f"Failed auth attempt from {request.client.host}")
         raise HTTPException(status_code=403, detail="Forbidden")
 
     target = call.target
 
     if target.service not in MAPPINGS:
+        logger.warning(f"Service not found. | SERVICE_NAME: {target.service}, ACTION: {target.action}, REPLY_TO: {call.caller}")
         return Reply(
                 service=target.service,
                 reply_to=call.caller,
@@ -57,6 +59,7 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
             ) 
 
     if target.action not in MAPPINGS[target.service]["actions"]:
+        logger.warning(f"Action not found. | ACTION: {target.action}, SERVICE: {target.service}, REPLY_TO: {call.caller}")
         return Reply(
             service=target.service,
             reply_to=call.caller,
@@ -65,6 +68,8 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
                 "reason": f"Action {target.action} not found in {target.service} service."
             }
         )
+
+    logger.info(f"Executing {target.action} on {target.service} for {call.caller}")
 
     cmd = [sys.executable, "-m", MAPPINGS[target.service]["path"], target.action, "--json"]
 
@@ -96,6 +101,7 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
                 await proc.wait()
             except ProcessLookupError:
                 pass
+        logger.error(f"Process timed out: {target.service} | ACTION: {target.action}")
         return Reply(
             service=target.service,
             reply_to=call.caller,
@@ -111,6 +117,7 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
                 await proc.wait()
             except ProcessLookupError:
                 pass
+        logger.error(f"Execution error of {target.service} | {e}")
         return Reply(
             service=target.service,
             reply_to=call.caller,
@@ -121,6 +128,7 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
         )
 
     status = "ok" if returncode == 0 else "error"
+    logger.info(f"Reply sent. For more details, look at logs/bunker.log | SERVICE: {target.service}, ACTION: {target.action}, STATUS: {status}, REPLY_TO: {call.caller}")
     return Reply(
         service=target.service,
         reply_to=call.caller,

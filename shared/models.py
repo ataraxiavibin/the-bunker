58a8c3aaa45f509e35124bc4706231c44ae1a3cd
Ataraxia <aixarata@proton.me>
2026-06-24 23:32:56 +0200

    refactor(models): implement discriminated unions for Event/Reply

--- shared/models.py
+++ shared/models.py
@@ -1,22 +1,66 @@
 # models.py
-from pydantic import BaseModel
-from typing import Dict, Any
+from pydantic import BaseModel, Field
+from typing import Dict, Any, Literal, Union, Annotated, Optional
+from datetime import datetime
 
-class Event(BaseModel):
-    service: str
-    status: str
-    payload: Dict[str, Any]
+# -------
+# Event
+# -------
 
-class Target(BaseModel):
+class EventBase(BaseModel):
     service: str
     action: str
+    stderr: str
+    timestamp: datetime
+    duration_ms: int
+
+class EventOk(EventBase):
+    status: Literal["ok"] = "ok"
+    payload: Dict[str, Any]
+
+class EventError(EventBase):
+    status: Literal["error", "fatal"] = "error"
+    returncode: int
+    reason: Optional[str]
+    payload: Optional[Dict[str, Any]]
+
+
+# discriminate by status, because it's the common field
+Event = Annotated[Union[EventOk, EventError], Field(discriminator="status")]
+
+
+# -------
+# Call
+# -------
 
 class Call(BaseModel):
     caller: str
     target: Target
 
-class Reply(BaseModel):
+class Target(BaseModel):
     service: str
+    action: Optional[str] = None
+
+# -------
+# Reply
+# -------
+
+class ReplyBase(BaseModel):
+    service: str
+    action: str
+    stderr: str
     reply_to: str
-    status: str
+    timestamp: datetime
+    duration_ms: int
+
+class ReplyOk(ReplyBase):
+    status: Literal["ok"] = "ok"
     payload: Dict[str, Any]
+
+class ReplyError(ReplyBase):
+    status: Literal["error", "fatal"] = "error"
+    returncode: int
+    reason: Optional[str]
+    payload: Optional[Dict[str, Any]]
+
+Reply = Annotated[Union[ReplyOk, ReplyError], Field(discriminator="status")]

0eb6e7b7ecb65b08fa0cd67ca4330e275c17a6d5
Ataraxia <aixarata@proton.me>
2026-06-28 18:18:44 +0200

    refactor(agent): introduce context builders
    both Event and Reply are now created and logged through Event/ReplyContext,
    increases readability and reduces code duplication

--- agent.py
+++ agent.py
@@ -49,9 +49,123 @@ class ExecutionResult(TypedDict):
     reason: Optional[str]
 
 
+class ReplyContext:
+    def __init__(self, call: Call):
+        self.call = call
+        self.service = call.target.service
+        self.action = call.target.action
+        # self._start_time = time.perf_counter()
+        # i may use that, but i think it's useless since i get time of the whole request path in bot
+        # and only execution time is also better for diagnostics later (maybe add avg exec time in bunker db)
+
+    def fatal(self, reason: str, stderr: str = "", returncode: int = -1, duration_ms: int = 0, payload: dict | None = None, log_details: str = None) -> ReplyError:
+        log_msg = f"[FATAL] {reason} | SERVICE: {self.service}, ACTION: {self.action}, REPLY_TO: {self.call.caller}"
+
+        if log_details:
+            log_msg += f"\n--- [DEBUG DETAILS] ---\n{log_details}\n------------------"
+
+        logger.error(log_msg)
+
+        return ReplyError(
+            service=self.service,
+            action=self.action,
+            reply_to=self.call.caller,
+            status="fatal",
+            stderr=stderr,
+            returncode=returncode,
+            reason=reason,
+            timestamp=datetime.now(timezone.utc),
+            duration_ms=duration_ms,
+            payload=payload
+        )
+
+    def error(self, reason: str, stderr: str, returncode: int, duration_ms: int, payload: dict | None = None) -> ReplyError:
+        logger.warning(f"[ERROR] {reason} | SERVICE: {self.service}, ACTION: {self.action}, REPLY_TO: {self.call.caller}")
+
+        return ReplyError(
+            service=self.service,
+            action=self.action,
+            reply_to=self.call.caller,
+            status="error",
+            stderr=stderr,
+            returncode=returncode,
+            reason=reason,
+            timestamp=datetime.now(timezone.utc),
+            duration_ms=duration_ms,
+            payload=payload
+        )
+
+    def ok(self, stderr: str, duration_ms: int, payload: dict) -> ReplyOk:
+        logger.info(f"successfull execution of {self.service}: {self.action} | REPLY_TO: {self.call.caller}")
+
+        return ReplyOk(
+            service=self.service,
+            action=self.action,
+            reply_to=self.call.caller,
+            status="ok",
+            stderr=stderr,
+            timestamp=datetime.now(timezone.utc),
+            duration_ms=duration_ms,
+            payload=payload
+            )
+
+
+class EventContext:
+    def __init__(self, target: Target):
+        self.target = target
+        self.service = target.service
+        self.action = target.action or ""
+
+    def fatal(self, reason: str, stderr: str = "", returncode: int = -1, duration_ms: int = 0, payload: Dict | None = None, log_details: str = None) -> EventError:
+        log_msg = f"[FATAL] {reason} | SERVICE: {self.service}, ACTION: {self.action}"
+
+        if log_details:
+            log_msg += f"\n--- [DEBUG DETAILS] ---\n{log_details}\n------------------"
+
+        logger.error(log_msg)
+
+        return EventError(
+            service=self.service,
+            action=self.action,
+            status="fatal",
+            stderr=stderr,
+            returncode=returncode,
+            reason=reason,
+            timestamp=datetime.now(timezone.utc),
+            duration_ms=duration_ms,
+            payload=payload
+        )
+
+    def error(self, reason: str, stderr: str, returncode: int, duration_ms: int, payload: dict | None = None) -> EventError:
+        logger.warning(f"[ERROR] {reason} | SERVICE: {self.service}, ACTION: {self.action}")
+
+        return EventError(
+            service=self.service,
+            action=self.action,
+            status="error",
+            stderr=stderr,
+            returncode=returncode,
+            reason=reason,
+            timestamp=datetime.now(timezone.utc),
+            duration_ms=duration_ms,
+            payload=payload
+        )
+
+    def ok(self, stderr: str, duration_ms: int, payload: dict) -> EventOk:
+        logger.info(f"successfull execution of {self.service}: {self.action}") # yeah, not really clear how does one see that it's event, not reply.
+
+        return EventOk(
+            service=self.service,
+            action=self.action,
+            status="ok",
+            stderr=stderr,
+            timestamp=datetime.now(timezone.utc),
+            duration_ms=duration_ms,
+            payload=payload
+        )
+
+
 async def execute_service(cmd: list, timeout: float = 15.0) -> ExecutionResult:
-    # it's a stdout/stderr we are looking for.
-    # transforming it into event/reply is gonna be original caller-functions duty.
     start_time = time.perf_counter()
     proc = None
 
@@ -132,46 +246,16 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
         logger.warning(f"Failed auth attempt from '{request.client.host}'")
         raise HTTPException(status_code=403, detail="Forbidden")
 
+    ctx = ReplyContext(call)
     target = call.target
+
     logger.info(f"Received call: '{target.action}' on {target.service} for {call.caller}.")
 
     if target.service not in MAPPINGS:
-        reason = f"service '{target.service}' not found."
-        logger.warning(f"[FATAL] {reason} | ACTION: {target.action}, REPLY_TO: {call.caller}")
-        return ReplyError(
-                service=target.service,
-                action=target.action,
-                reply_to=call.caller,
-
-                status="fatal",
-                stderr="",
-                returncode=-1,
-                reason=reason,
-
-                timestamp=datetime.now(timezone.utc),
-                duration_ms=0,
-
-                payload=None
-        )
+        return ctx.fatal(f"service '{target.service}' not found.")
 
     if target.action not in MAPPINGS[target.service]["actions"]:
-        reason = f"action '{target.action}' not found in '{target.service}' service."
-        logger.warning(f"[FATAL] {reason} | REPLY_TO: {call.caller}")
-        return ReplyError(
-                service=target.service,
-                action=target.action,
-                reply_to=call.caller,
-
-                status="fatal",
-                stderr="",
-                returncode=-1,
-                reason=reason,
-
-                timestamp=datetime.now(timezone.utc),
-                duration_ms=0,
-
-                payload=None
-        )
+        return ctx.fatal(f"action '{target.action}' not found in '{target.service}' service.")
 
     logger.info(f"Executing '{target.action}' on {target.service} for {call.caller}.")
 
@@ -188,68 +272,34 @@ async def run_call(call: Call, request: Request, x_token: str = Header(...)) -> 
 
     parsed_stdout = None
 
-    if stdout:
-        try:
-            parsed_stdout = json.loads(stdout)
-        except json.JSONDecodeError as e:
-            logger.critical(f"[FATAL] An error occured while parsing JSON | (unparsed) STDOUT: {stdout}")
-            return ReplyError(
-                service=target.service,
-                action=target.action,
-                reply_to=call.caller,
+    # 1. guarantee, that stdout is there
+    if not stdout:
+        return ctx.fatal(reason or "service returned empty stdout", stderr=stderr, returncode=returncode, duration_ms=duration_ms)
 
-                status="fatal",
-                stderr=stderr,
-                returncode=returncode,
-                reason=f"failed to parse service stdout; more at logs/agent.log",
-                timestamp=datetime.now(timezone.utc),
+    # 2. guarantee, that stdout is json
+    try:
+        parsed_stdout = json.loads(stdout)
+    except json.JSONDecodeError as e:
+        return ctx.fatal(f"an error occured while parsing json", stderr=stderr, returncode=returncode, duration_ms=duration_ms, log_details=stdout)
 
-                duration_ms=duration_ms,
-                payload=None
-            )
-
+    # 3. if there's stdout, it's json.
     if returncode == 0:
-        logger.info(f"Successfull execution of {target.service}: {target.action} | REPLY_TO: {call.caller}")
-        return ReplyOk(
-            # metadata
-            service=target.service,
-            action=target.action,
-            reply_to=call.caller,
-            # diagnostic
-            status="ok",
-            stderr=stderr,
-            timestamp=datetime.now(timezone.utc),
-            # used data
-            duration_ms=duration_ms,
-            payload=parsed_stdout
-        )
+        return ctx.ok(stderr=stderr, duration_ms=duration_ms, payload=parsed_stdout)
     else:
         if not reason and stderr:
             reason = stderr.strip().split('\n')[-1][:100]
         if not reason:
-            reason = parsed_stdout.get("message", f"service exited with {returncode}")
+            reason = parsed_stdout.get("message", f"service exited with {returncode}") # in case of failure, services should print in stdout reason in "message"?
 
-        logger.warning(f"[FATAL]: Error from service {target.service}: {target.action} | REASON: {reason}, REPLY_to: {call.caller}")
-        return ReplyError(
-                service=target.service,
-                action=target.action,
-                reply_to=call.caller,
+        logger.warning(f"Error from service {target.service}: {target.action} | REASON: {reason}, REPLY_to: {call.caller}")
+        return ctx.error(stderr=stderr, returncode=returncode, reason=reason, duration_ms=duration_ms, payload=parsed_stdout)
 
-                status="fatal",
-                stderr=stderr,
-                returncode=returncode,
-                reason=reason,
-
-                timestamp=datetime.now(timezone.utc),
-                duration_ms=duration_ms,
-                payload=parsed_stdout
-        )
 
 async def handle_cli():
     parser = argparse.ArgumentParser(
         prog="agent",
         description="runs local services",
-        exit_on_error=False
+        exit_on_error=True
     )
 
 
